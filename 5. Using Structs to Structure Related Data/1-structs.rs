1ee29f3f6b632f07c643f8f0915e170b60b0f5dc
Phoenix <charlesch129@protonmail.com>
2023-08-14 19:29:32 +0000

    Updating local git

--- 5. Using Structs to Structure Related Data/1-structs.rs
+++ 5. Using Structs to Structure Related Data/1-structs.rs
@@ -4,3 +4,23 @@ struct User {
     email: String,
     sign_in_count: u64,
 }
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}

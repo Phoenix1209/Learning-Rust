d5495142eece5025565d4f60ef195a56bc70dc13
Phoenix <charlesch129@protonmail.com>
2023-12-18 15:08:26 -0500

    Updating Files v2

--- 6.  Enums and Pattern Matching/7-same_but_with_struct.rs
+++ 6.  Enums and Pattern Matching/7-same_but_with_struct.rs
@@ -1,7 +1,3 @@
-/*	But if we used the different structs, each of which has its own type,
-	we couldn’t as easily define a function to take any of these kinds of messages as
-	we could with the Message enum defined in Listing 6-2, which is a single type. */
-
 struct QuitMessage; // unit struct
 struct MoveMessage {
     x: i32,
@@ -11,3 +7,7 @@ struct WriteMessage(String); // tuple struct
 struct ChangeColorMessage(i32, i32, i32); // tuple struct
 
 fn main() {}
+
+/*	But if we used the different structs, each of which has its own type,
+	we couldn’t as easily define a function to take any of these kinds of messages as
+	we could with the Message enum defined in Listing 6-2, which is a single type. */

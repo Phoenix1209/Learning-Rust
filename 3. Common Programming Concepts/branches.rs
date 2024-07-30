446b038c1043e70ee8056fdafa50cfb06c30c858
Phoenix <charlesch129@protonmail.com>
2024-07-30 12:17:22 -0500

    Modifying some codes

--- 3. Common Programming Concepts/branches.rs
+++ 3. Common Programming Concepts/branches.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 fn main() {
     let number = 3;
 
@@ -7,14 +6,4 @@ fn main() {
     } else {
         println!("condition was false");
     }
-=======
-fn main() {
-    let number = 3;
-
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
->>>>>>> 50ab04f68128ae3a1f2ff698801afe071ff2b891
 }

dbf9b58a38ca4e6a184249f865e453440ebe773d
Charles <charlesch129@protonmail.com>
2024-08-02 03:44:54 -0500

    Finished Chapter 8 3/3

--- 6.  Enums and Pattern Matching/1. Defining a Enum/5-struct_inside_enum.rs
+++ 6.  Enums and Pattern Matching/1. Defining a Enum/5-struct_inside_enum.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #![allow(unused)]
 fn main() {
 	struct Ipv4Addr {
@@ -13,20 +12,4 @@ fn main() {
 		V4(Ipv4Addr),
 		V6(Ipv6Addr),
 	}
-=======
-#![allow(unused)]
-fn main() {
-	struct Ipv4Addr {
-		// --snip--
-	}
-
-	struct Ipv6Addr {
-		// --snip--
-	}
-
-	enum IpAddr {
-		V4(Ipv4Addr),
-		V6(Ipv6Addr),
-	}
->>>>>>> 50ab04f68128ae3a1f2ff698801afe071ff2b891
 }
